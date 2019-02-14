93aaf53427c48967e86d85f1ef6ce326d6f21576
xuqd <xuqd@getui.com>
2019-02-14 17:25:42 +0800

    support to query whois server when queried domain whose whois server is unknown.

--- query.py
+++ query.py
@@ -4,10 +4,28 @@
 
 import socket
 import json
+import re
 
 f = open('servers.json')
 servers = json.loads(f.read())
 f.close()
+
+
+def query_whois_server(top):
+    server = "whois.iana.org"
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(30)
+        s.connect((server, 43))
+        s.sendall(bytes(top + '\r\n', 'utf-8'))
+        data = s.recv(16 * 1024)
+    response = str(data, 'utf8')
+    matcher = re.search('whois:\s*(.+)', response)
+    if matcher:
+        print(matcher.group(1))
+        return matcher.group(1)
+    else:
+        return None
+    pass
 
 
 def query(domain):
@@ -16,7 +34,12 @@ def query(domain):
     if top in servers:
         query_server = servers[top]
     else:
-        query_server = "whois.internic.net"
+        query_server = query_whois_server(top)
+        servers[top] = query_server
+
+    if query_server is None:
+        print('*.{} is not supported!'.format(top))
+        return '*.{} is not supported!'.format(top)
 
     print('query ' + domain)
     print('querying on ' + query_server + ' ...')
@@ -32,5 +55,5 @@ def query(domain):
     return response
 
 
-# domain = 'xqd.one'
+# domain = '导航.中国'
 # query(domain)
